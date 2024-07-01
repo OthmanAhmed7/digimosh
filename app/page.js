98dc54e912826493b045675b103fbeff429dc444
Othman Ahmed <othmanahmed258@gmail.com>
2024-07-01 22:34:44 +0300

    edit Sec1.jsx import

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Form from "@/components/Form";
-import Sec1 from "@/components/sec1";
+import Sec1 from "@/components/Sec1";
 import Sec2 from "@/components/Sec2";
 import Sec3 from "@/components/Sec3";
 
